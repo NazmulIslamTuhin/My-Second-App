03895c0376c8b5a677e4e1928a2384bbacc6a7d0
NazmulIslamTuhin <nazmulislam640050@gmail.com>
2023-03-06 01:21:51 +0600

    font-size-customize

--- lib/main.dart
+++ lib/main.dart
@@ -256,7 +256,7 @@ class _MyHomePageState extends State<MyHomePage> {
                         color: Colors.grey[850],
                         borderRadius: BorderRadius.circular(40)
                       ),
-                      child: Center(child: Text(".", style: TextStyle(color: Colors.white, fontSize: 30, fontWeight: FontWeight.bold),)),
+                      child: Center(child: Text(".", style: TextStyle(color: Colors.white, fontSize: 40, fontWeight: FontWeight.bold),)),
                     ),
                     Container(
                       height: 80,
@@ -265,7 +265,7 @@ class _MyHomePageState extends State<MyHomePage> {
                         color: Colors.orange,
                         borderRadius: BorderRadius.circular(40)
                       ),
-                      child: Center(child: Text("=", style: TextStyle(color: Colors.white, fontSize: 30, fontWeight: FontWeight.bold),)),
+                      child: Center(child: Text("=", style: TextStyle(color: Colors.white, fontSize: 40, fontWeight: FontWeight.bold),)),
                     ),
                   ],
                 ),
