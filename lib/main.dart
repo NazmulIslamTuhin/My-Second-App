77ae90afc756c1419a07079956e3159fcdf76e8c
NazmulIslamTuhin <nazmulislam640050@gmail.com>
2023-03-07 10:32:30 +0600

    button-ontap-"÷"-done

--- lib/main.dart
+++ lib/main.dart
@@ -133,14 +133,21 @@ class _MyHomePageState extends State<MyHomePage> {
                         child: Center(child: Text("%", style: TextStyle(color: Colors.black, fontSize: 30, fontWeight: FontWeight.bold),)),
                       ),
                     ),
-                    Container(
-                      height: 80,
-                      width: 80,
-                      decoration: BoxDecoration(
-                          color: Colors.orange,
-                        borderRadius: BorderRadius.circular(40)
+                    InkWell(
+                      onTap: (){
+                        setState(() {
+                          value = "÷";
+                        });
+                      },
+                      child: Container(
+                        height: 80,
+                        width: 80,
+                        decoration: BoxDecoration(
+                            color: Colors.orange,
+                          borderRadius: BorderRadius.circular(40)
+                        ),
+                        child: Center(child: Text("÷", style: TextStyle(color: Colors.white, fontSize: 40, fontWeight: FontWeight.bold),)),
                       ),
-                      child: Center(child: Text("÷", style: TextStyle(color: Colors.white, fontSize: 40, fontWeight: FontWeight.bold),)),
                     ),
                   ],
                 ),
