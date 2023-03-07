730fa1ec583d4588f06598b66e76cf64f3fbff7b
NazmulIslamTuhin <nazmulislam640050@gmail.com>
2023-03-07 10:08:28 +0600

    button-ontap-"6"-done

--- lib/main.dart
+++ lib/main.dart
@@ -227,14 +227,21 @@ class _MyHomePageState extends State<MyHomePage> {
                         child: Center(child: Text("5", style: TextStyle(color: Colors.white, fontSize: 40, fontWeight: FontWeight.bold),)),
                       ),
                     ),
-                    Container(
-                      height: 80,
-                      width: 80,
-                      decoration: BoxDecoration(
-                          color: Colors.grey[850],
-                        borderRadius: BorderRadius.circular(40)
+                    InkWell(
+                      onTap: (){
+                        setState(() {
+                          value = "6";
+                        });
+                      },
+                      child: Container(
+                        height: 80,
+                        width: 80,
+                        decoration: BoxDecoration(
+                            color: Colors.grey[850],
+                          borderRadius: BorderRadius.circular(40)
+                        ),
+                        child: Center(child: Text("6", style: TextStyle(color: Colors.white, fontSize: 40, fontWeight: FontWeight.bold),)),
                       ),
-                      child: Center(child: Text("6", style: TextStyle(color: Colors.white, fontSize: 40, fontWeight: FontWeight.bold),)),
                     ),
                     Container(
                       height: 80,
