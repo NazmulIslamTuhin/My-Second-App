c0369da829fe2c02ae678feba6289e285722f764
NazmulIslamTuhin <nazmulislam640050@gmail.com>
2023-03-17 21:21:04 +0600

    calculator-design-done

--- lib/main.dart
+++ lib/main.dart
@@ -153,7 +153,7 @@ class _MyHomePageState extends State<MyHomePage> {
                             color: Colors.orange,
                           borderRadius: BorderRadius.circular(40)
                         ),
-                        child: Center(child: Text("÷", style: TextStyle(color: Colors.white, fontSize: 30, fontWeight: FontWeight.bold),)),
+                        child: Center(child: Text("÷", style: TextStyle(color: Colors.white, fontSize: 40, fontWeight: FontWeight.bold),)),
                       ),
                     ),
                   ],
@@ -416,7 +416,7 @@ class _MyHomePageState extends State<MyHomePage> {
                           color: Colors.orange,
                           borderRadius: BorderRadius.circular(40)
                         ),
-                        child: Center(child: Text("=", style: TextStyle(color: Colors.white, fontSize: 40, fontWeight: FontWeight.bold),)),
+                        child: Center(child: Text("=", style: TextStyle(color: Colors.white, fontSize: 50, fontWeight: FontWeight.bold),)),
                       ),
                     ),
                   ],
