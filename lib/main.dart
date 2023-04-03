4ae4e0b7e20b64472d7b43ef4e64dbc4d198f14f
NazmulIslamTuhin <nazmulislam640050@gmail.com>
2023-04-04 01:33:45 +0600

    2-container-design-part-done

--- lib/main.dart
+++ lib/main.dart
@@ -248,27 +248,24 @@ class _MyHomePageState extends State<MyHomePage> {
 
                    ),
                   SizedBox(height: 0.5,),
-                  Row(
-                    children: [
-                      Container(
-                        height: 50,
-                        width: 350,
-                        decoration: BoxDecoration(
-                            color: Colors.black12,
-                            borderRadius: BorderRadius.circular(10)
-                        ),
-                        padding: const EdgeInsets.all(10),
-                        child: Column(
-                            mainAxisAlignment: MainAxisAlignment.start,
-                            children:[
-                              Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                                  children:[
-                                    Center(child: Text("Friday", style: TextStyle(color: Colors.black, fontSize: 20, fontWeight: FontWeight.bold),),
-                                    ),Text("31°--35°", style: TextStyle(color: Colors.black, fontSize: 20, fontWeight: FontWeight.bold),),
-                                  ]),
-                            ]),
-                      ),
-                    ],
+                  Container(
+                    height: 50,
+                    width: 350,
+                    decoration: BoxDecoration(
+                        color: Colors.black12,
+                        borderRadius: BorderRadius.circular(10)
+                    ),
+                    padding: const EdgeInsets.all(10),
+                    child: Column(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children:[
+                          Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                              children:[
+                                Center(child: Text("Friday", style: TextStyle(color: Colors.black, fontSize: 20, fontWeight: FontWeight.bold),),
+                                ),Text("26°--32°", style: TextStyle(color: Colors.black, fontSize: 20, fontWeight: FontWeight.bold),),
+                              ]),
+                        ]),
+
                   ),
 
                   SizedBox(height: 30,),
@@ -276,33 +273,33 @@ class _MyHomePageState extends State<MyHomePage> {
 
 
 
-                  // Row(mainAxisAlignment: MainAxisAlignment.start,
-                  //   children: [
-                  //     Container(
-                  //       height: 180,
-                  //       width: 170,
-                  //       decoration: BoxDecoration(
-                  //           color: Colors.blue,
-                  //           borderRadius: BorderRadius.circular(10)
-                  //       ),
-                  //       padding: const EdgeInsets.all(10),
-                  //       child: Center(child: Text("1", style: TextStyle(color: Colors.black, fontSize: 90, fontWeight: FontWeight.bold),)),
-                  //     ),
-                  //     SizedBox(width: 15,),
-                  //     Container(
-                  //
-                  //       height: 180,
-                  //       width: 170,
-                  //       decoration: BoxDecoration(
-                  //           color: Colors.blue,
-                  //           borderRadius: BorderRadius.circular(10)
-                  //       ),
-                  //
-                  //       padding: const EdgeInsets.all(10),
-                  //       child: Center(child: Text("2", style: TextStyle(color: Colors.black, fontSize: 90, fontWeight: FontWeight.bold),)),
-                  //     ),
-                  //   ],
-                  // ),
+                  Row(mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      Container(
+                        height: 180,
+                        width: 170,
+                        decoration: BoxDecoration(
+                            color: Colors.blue,
+                            borderRadius: BorderRadius.circular(10)
+                        ),
+                        padding: const EdgeInsets.all(10),
+                        child: Center(child: Text("1", style: TextStyle(color: Colors.black, fontSize: 90, fontWeight: FontWeight.bold),)),
+                      ),
+                      SizedBox(width: 15,),
+                      Container(
+
+                        height: 180,
+                        width: 170,
+                        decoration: BoxDecoration(
+                            color: Colors.blue,
+                            borderRadius: BorderRadius.circular(10)
+                        ),
+
+                        padding: const EdgeInsets.all(10),
+                        child: Center(child: Text("2", style: TextStyle(color: Colors.black, fontSize: 90, fontWeight: FontWeight.bold),)),
+                      ),
+                    ],
+                  ),
 
 
 
