2701d3a51b399679203c361adf89d7ec07493155
NazmulIslamTuhin <nazmulislam640050@gmail.com>
2023-04-06 05:32:29 +0600

    weakly-container-spaceAround

--- lib/main.dart
+++ lib/main.dart
@@ -145,7 +145,7 @@ class _MyHomePageState extends State<MyHomePage> {
                     child: Column(
                         mainAxisAlignment: MainAxisAlignment.center,
                         children:[
-                          Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          Row(mainAxisAlignment: MainAxisAlignment.spaceAround,
                               children:[
                                 Center(child: Text("Saturday", style: TextStyle(color: Colors.black, fontSize: 20, fontWeight: FontWeight.bold),),
                                 ),Text("31°--35°", style: TextStyle(color: Colors.black, fontSize: 20, fontWeight: FontWeight.bold),),
