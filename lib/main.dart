046e8ddd5b8d4499a497259525ddcbb5fd287afe
NazmulIslamTuhin <nazmulislam640050@gmail.com>
2023-04-06 05:52:24 +0600

    text-issu

--- lib/main.dart
+++ lib/main.dart
@@ -313,7 +313,7 @@ class _MyHomePageState extends State<MyHomePage> {
                               Row(mainAxisAlignment: MainAxisAlignment.spaceAround ,
                                   children:[
                                     Center(child: Text("  SUNRISE", style: TextStyle(color: Colors.black, fontSize: 20, fontWeight: FontWeight.bold),)),
-                                    SizedBox(width: 10,),
+                                    SizedBox( height: 100,),
                                     Center(child: Text("5 AM", style: TextStyle(color: Colors.black, fontSize: 20, fontWeight: FontWeight.bold,),)),
                                   ]),
                             ]),
