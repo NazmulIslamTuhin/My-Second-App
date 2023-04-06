f9d28eb68edb0bdc95926c8a548dc774c41bb314
NazmulIslamTuhin <nazmulislam640050@gmail.com>
2023-04-07 01:43:35 +0600

    uv-container-style-start

--- lib/main.dart
+++ lib/main.dart
@@ -295,11 +295,15 @@ class _MyHomePageState extends State<MyHomePage> {
                             borderRadius: BorderRadius.circular(10)
                         ),
                         padding: const EdgeInsets.all(10),
-                        child: Column(mainAxisAlignment: MainAxisAlignment.start,
+                        child: Row(mainAxisAlignment: MainAxisAlignment.start,
                         children: [
                           Center(child: Text("UV INDEX", style: TextStyle(color: Colors.black, fontSize: 20, fontWeight: FontWeight.bold),)),
-                          Center(child: Text("Low", style: TextStyle(color: Colors.pink, fontSize: 30),)),
-                          Text("use sunprotection 9am to 12pm")
+                          SizedBox(height: 10,),
+                          Center(child: Text("Low", style: TextStyle(color: Colors.pink, fontSize: 30,fontWeight: FontWeight.bold),)),
+                          SizedBox(height: 0,),
+
+                          Text("use sun protextion 9 am to 2pm"),
+                          Text("use sunprotection 9am to 12pm",)
                         ],
                         ),
 
