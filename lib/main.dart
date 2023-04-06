12bdd0646f5743ac62e6f8864a78ddcd04b3a531
NazmulIslamTuhin <nazmulislam640050@gmail.com>
2023-04-06 22:55:36 +0600

    preciption-container-style-end-done

--- lib/main.dart
+++ lib/main.dart
@@ -295,7 +295,7 @@ class _MyHomePageState extends State<MyHomePage> {
                             borderRadius: BorderRadius.circular(10)
                         ),
                         padding: const EdgeInsets.all(10),
-                        child: Column(mainAxisAlignment: MainAxisAlignment.center,
+                        child: Column(mainAxisAlignment: MainAxisAlignment.start,
                         children: [
                           Center(child: Text("UV INDEX", style: TextStyle(color: Colors.black, fontSize: 20, fontWeight: FontWeight.bold),)),
                         ],
@@ -327,19 +327,19 @@ class _MyHomePageState extends State<MyHomePage> {
                       //       ]),
                       //
                       // ),
-                      // Container(
-                      //
-                      //   height: 180,
-                      //   width: 170,
-                      //   decoration: BoxDecoration(
-                      //       color: Colors.blue,
-                      //       borderRadius: BorderRadius.circular(10)
-                      //   ),
-                      //
-                      //   padding: const EdgeInsets.all(10),
-                      //   child: Center(child: Text("SUNRISE",style: TextStyle(color: Colors.black, fontSize: 15, fontWeight: FontWeight.bold),),),
-                      //
-                      // ),
+                      Container(
+
+                        height: 180,
+                        width: 170,
+                        decoration: BoxDecoration(
+                            color: Colors.blue,
+                            borderRadius: BorderRadius.circular(10)
+                        ),
+
+                        padding: const EdgeInsets.all(10),
+                        child: Center(child: Text("SUNRISE",style: TextStyle(color: Colors.black, fontSize: 15, fontWeight: FontWeight.bold),),),
+
+                      ),
                     ],
                   ),
 
