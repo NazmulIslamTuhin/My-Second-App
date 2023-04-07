ae70c0f678c9fdfd5d7a1f5b73030e4fac5e2850
NazmulIslamTuhin <nazmulislam640050@gmail.com>
2023-04-07 22:55:26 +0600

    text-change

--- lib/main.dart
+++ lib/main.dart
@@ -299,7 +299,7 @@ class _MyHomePageState extends State<MyHomePage> {
                         children: [
                           Center(child: Text("UV INDEX", style: TextStyle(color: Colors.lightGreenAccent, fontSize: 20, fontWeight: FontWeight.bold),)),
                           SizedBox(height: 10,),
-                          Center(child: Text("Low", style: TextStyle(color: Colors.pink, fontSize: 30,fontWeight: FontWeight.bold),)),
+                          Center(child: Text("Low", style: TextStyle(color: Colors.red, fontSize: 30,fontWeight: FontWeight.bold),)),
                           SizedBox(height: 10,),
 
                           Text("Use sun protextion 9 am to 2pm", style: TextStyle(fontSize: 17, fontWeight: FontWeight.bold),),
