d26615d098592ef2f9b111fd6982da6b2e1b1c38
NazmulIslamTuhin <nazmulislam640050@gmail.com>
2023-04-10 10:14:22 +0600

    container-ready

--- lib/main.dart
+++ lib/main.dart
@@ -331,10 +331,112 @@ class _MyHomePageState extends State<MyHomePage> {
                             ]),
 
                       ),
-
-
                     ],
                   ),
+                  SizedBox(height: 10,),
+                  // ************************
+                  Row(mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      Container(
+                        height: 180,
+                        width: 170,
+                        decoration: BoxDecoration(
+                            color: Colors.blue,
+                            borderRadius: BorderRadius.circular(10)
+                        ),
+                        padding: const EdgeInsets.all(10),
+                        child: Column(mainAxisAlignment: MainAxisAlignment.start,
+                          children: [
+                            Center(child: Text("UV INDEX", style: TextStyle(color: Colors.lightGreenAccent, fontSize: 20, fontWeight: FontWeight.bold),)),
+                            SizedBox(height: 10,),
+                            Center(child: Text("Low", style: TextStyle(color: Colors.red, fontSize: 30,fontWeight: FontWeight.bold),)),
+                            SizedBox(height: 10,),
+
+                            Text("Use sun protextion 9 am to 2pm", style: TextStyle(fontSize: 17, fontWeight: FontWeight.bold),),
+                          ],
+                        ),
+                      ),
+                      SizedBox(width: 10,),
+
+
+                      Container(
+                        height: 180,
+                        width: 170,
+                        decoration: BoxDecoration(
+                            color: Colors.blue,
+                            borderRadius: BorderRadius.circular(10)
+                        ),
+                        padding: const EdgeInsets.all(10),
+                        child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children:[
+                              Column(mainAxisAlignment: MainAxisAlignment.spaceAround ,
+                                  children:[
+                                    Center(child: Text("  SUNSET", style: TextStyle(color: Colors.black, fontSize: 20, fontWeight: FontWeight.bold),)),
+                                    SizedBox( height: 50,),
+                                    Center(child: Text("6:21 PM", style: TextStyle(color: Colors.white, fontSize: 35, fontWeight: FontWeight.bold,),)),
+                                    SizedBox(height: 30,),
+                                    Center(child: Text("SUNRISE: 5:50AM", style: TextStyle(color: Colors.black, fontSize: 12, fontWeight: FontWeight.bold),))
+                                  ]),
+                            ]),
+
+                      ),
+                    ],
+                  ),
+                  // ************************
+                  SizedBox(height: 10,),
+                  // **********************2
+                  Row(mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      Container(
+                        height: 180,
+                        width: 170,
+                        decoration: BoxDecoration(
+                            color: Colors.blue,
+                            borderRadius: BorderRadius.circular(10)
+                        ),
+                        padding: const EdgeInsets.all(10),
+                        child: Column(mainAxisAlignment: MainAxisAlignment.start,
+                          children: [
+                            Center(child: Text("UV INDEX", style: TextStyle(color: Colors.lightGreenAccent, fontSize: 20, fontWeight: FontWeight.bold),)),
+                            SizedBox(height: 10,),
+                            Center(child: Text("Low", style: TextStyle(color: Colors.red, fontSize: 30,fontWeight: FontWeight.bold),)),
+                            SizedBox(height: 10,),
+
+                            Text("Use sun protextion 9 am to 2pm", style: TextStyle(fontSize: 17, fontWeight: FontWeight.bold),),
+                          ],
+                        ),
+                      ),
+                      SizedBox(width: 10,),
+
+
+                      Container(
+                        height: 180,
+                        width: 170,
+                        decoration: BoxDecoration(
+                            color: Colors.blue,
+                            borderRadius: BorderRadius.circular(10)
+                        ),
+                        padding: const EdgeInsets.all(10),
+                        child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children:[
+                              Column(mainAxisAlignment: MainAxisAlignment.spaceAround ,
+                                  children:[
+                                    Center(child: Text("  SUNSET", style: TextStyle(color: Colors.black, fontSize: 20, fontWeight: FontWeight.bold),)),
+                                    SizedBox( height: 50,),
+                                    Center(child: Text("6:21 PM", style: TextStyle(color: Colors.white, fontSize: 35, fontWeight: FontWeight.bold,),)),
+                                    SizedBox(height: 30,),
+                                    Center(child: Text("SUNRISE: 5:50AM", style: TextStyle(color: Colors.black, fontSize: 12, fontWeight: FontWeight.bold),))
+                                  ]),
+                            ]),
+
+                      ),
+                    ],
+                  ),
+                  // *************************2
+
+
                   SizedBox(height: 10,),
                   Row(mainAxisAlignment: MainAxisAlignment.start,
                     children: [
