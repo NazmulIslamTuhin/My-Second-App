460ca3319586e0fc3d9cfe2cbff7241f5c99b2cf
NazmulIslamTuhin <nazmulislam640050@gmail.com>
2023-04-10 10:35:43 +0600

    short-container-text-problem-solving-start

--- lib/main.dart
+++ lib/main.dart
@@ -297,7 +297,7 @@ class _MyHomePageState extends State<MyHomePage> {
                         padding: const EdgeInsets.all(10),
                         child: Column(mainAxisAlignment: MainAxisAlignment.start,
                         children: [
-                          Center(child: Text("UV INDEX", style: TextStyle(color: Colors.lightGreenAccent, fontSize: 20, fontWeight: FontWeight.bold),)),
+                          Text("UV INDEX", style: TextStyle(color: Colors.lightGreenAccent, fontSize: 15, fontWeight: FontWeight.bold),),
                           SizedBox(height: 10,),
                           Center(child: Text("Low", style: TextStyle(color: Colors.red, fontSize: 30,fontWeight: FontWeight.bold),)),
                           SizedBox(height: 10,),
