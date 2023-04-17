1946e08c36711294afae16ee99dedd94e871aea5
NazmulIslamTuhin <nazmulislam640050@gmail.com>
2023-04-17 21:54:16 +0600

    wind-container-problem-solve

--- lib/main.dart
+++ lib/main.dart
@@ -439,7 +439,7 @@ class _MyHomePageState extends State<MyHomePage> {
                                 SizedBox(width: 100,),
                                 Row(mainAxisAlignment: MainAxisAlignment.center,
                                   children: [
-                                    Text("E", style: TextStyle(fontSize: 25,fontWeight: FontWeight.bold, color: Colors.purpleAccent),),
+                                    Text("t", style: TextStyle(fontSize: 25,fontWeight: FontWeight.bold, color: Colors.purpleAccent),),
                                   ],
                                 ),
                               ],
@@ -520,7 +520,7 @@ class _MyHomePageState extends State<MyHomePage> {
                     height: 150,
                     width: 350,
                     decoration: BoxDecoration(
-                      color: Colors.black12,
+                      color: Colors.black,
                       borderRadius: BorderRadius.circular(10)
                     ),
                     padding: EdgeInsets.all(10),
