41607e24c6358f7290b57644adb0011bd8cf343e
NazmulIslamTuhin <nazmulislam640050@gmail.com>
2023-04-30 00:14:41 +0600

    monday-box-color

--- lib/main.dart
+++ lib/main.dart
@@ -189,7 +189,7 @@ class _MyHomePageState extends State<MyHomePage> {
                         children:[
                           Row(mainAxisAlignment: MainAxisAlignment.spaceAround,
                               children:[
-                                Center(child: Text("Monday", style: TextStyle(color: Colors.black, fontSize: 20, fontWeight: FontWeight.bold),),
+                                Center(child: Text("Monday", style: TextStyle(color: Colors.white70, fontSize: 20, fontWeight: FontWeight.bold),),
                                 ),Text("33°--34°", style: TextStyle(color: Colors.black, fontSize: 20, fontWeight: FontWeight.bold),),
                               ]),
                         ]), ),
