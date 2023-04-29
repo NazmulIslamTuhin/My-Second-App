c75004edb044d9ada0aafa64ed61d2d3fa1dd487
NazmulIslamTuhin <nazmulislam640050@gmail.com>
2023-04-30 00:13:54 +0600

    sunday-box-color

--- lib/main.dart
+++ lib/main.dart
@@ -172,7 +172,7 @@ class _MyHomePageState extends State<MyHomePage> {
                           Row(mainAxisAlignment: MainAxisAlignment.spaceAround,
                               children:[
                                 Center(child: Text("Sunday", style: TextStyle(color: Colors.white70, fontSize: 20, fontWeight: FontWeight.bold),),
-                                ),Text("24°--34°", style: TextStyle(color: Colors.black, fontSize: 20, fontWeight: FontWeight.bold),),
+                                ),Text("24°--34°", style: TextStyle(color: Colors.white70, fontSize: 20, fontWeight: FontWeight.bold),),
                               ]),
                         ]),),
                   SizedBox(height: 0.5,),
