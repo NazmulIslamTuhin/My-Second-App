ab8e7fd4925fe6ff6df7ba8643e4d9ce92fa8967
NazmulIslamTuhin <nazmulislam640050@gmail.com>
2023-04-30 00:20:34 +0600

    tuesday-box-color

--- lib/main.dart
+++ lib/main.dart
@@ -193,7 +193,7 @@ class _MyHomePageState extends State<MyHomePage> {
                                 ),Text("33°--34°", style: TextStyle(color: Colors.white70, fontSize: 20, fontWeight: FontWeight.bold),),
                               ]),
                         ]), ),
-                  SizedBox(height: 0.5,),
+                  SizedBox(height: 3,),
                   Container(
                     height: 50,
                     width: 350,
