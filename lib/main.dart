3383e6047fa7e560e8f816e3fbaa21a295568a9d
NazmulIslamTuhin <nazmulislam640050@gmail.com>
2023-05-02 22:19:39 +0600

    report-an-issu-color-change

--- lib/main.dart
+++ lib/main.dart
@@ -116,7 +116,7 @@ class _MyHomePageState extends State<MyHomePage> {
                         borderRadius: BorderRadius.circular(10)
                     ),
                     padding: const EdgeInsets.all(10),
-                    child: Center(child: const Text("10-day Forecast", style: TextStyle(color: Colors.white70, fontSize: 18, fontWeight: FontWeight.bold),)),
+                    child: Center(child: const Text("10-day Forecast", style: TextStyle(color: Colors.white70, fontSize: 15, fontWeight: FontWeight.bold),)),
                   ),
                   SizedBox(height: 12,),
                   Container(
