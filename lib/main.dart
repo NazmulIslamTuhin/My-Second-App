1b6caaf7ddf2844bc20d65de32bca15cfaf3de71
NazmulIslamTuhin <nazmulislam640050@gmail.com>
2023-05-06 18:04:47 +0600

    humidity-container-problem-solve

--- lib/main.dart
+++ lib/main.dart
@@ -408,7 +408,7 @@ class _MyHomePageState extends State<MyHomePage> {
                                        Text("HUMIDITY", style: TextStyle(color: Colors.black54, fontSize: 15, fontWeight: FontWeight.bold),),
                                      ],
                                    ),
-                                    SizedBox( height: 30,),
+                                    SizedBox( height: 20,),
                                     Center(child: Text("39%", style: TextStyle(color: Colors.yellow, fontSize: 50, fontWeight: FontWeight.bold,),)),
                                     SizedBox(height: 15,),
                                     Row(mainAxisAlignment: MainAxisAlignment.center,
