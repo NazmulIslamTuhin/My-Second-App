4babab0298272ba2cc29bc5afa86a4363e095f82
NazmulIslamTuhin <nazmulislam640050@gmail.com>
2023-05-13 09:50:31 +0600

    text-pixel-problme

--- lib/main.dart
+++ lib/main.dart
@@ -952,7 +952,7 @@ class _MyHomePageState extends State<MyHomePage> {
                           mainAxisAlignment: MainAxisAlignment.center,
                           children: [
                             Text(
-                              "Report an Issue fshevbrjqwnbmlkjhgffdsapiuyt",
+                              "Report an Issue fshevbrjqwnbmlkjhgffdsapiuytr",
                               style: TextStyle(
                                   fontSize: 25,
                                   color: Colors.red,
