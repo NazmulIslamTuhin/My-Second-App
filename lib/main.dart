f895625371af894fbf3e7ae7fda1ccfda16b806a
NazmulIslamTuhin <nazmulislam640050@gmail.com>
2023-05-13 09:46:56 +0600

    text-poxel-problme

--- lib/main.dart
+++ lib/main.dart
@@ -952,7 +952,7 @@ class _MyHomePageState extends State<MyHomePage> {
                           mainAxisAlignment: MainAxisAlignment.center,
                           children: [
                             Text(
-                              "Report an Issue",
+                              "Report an Issue ",
                               style: TextStyle(
                                   fontSize: 25,
                                   color: Colors.red,
