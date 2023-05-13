2f89b10aaf647e02b8404ff655996d7af1c173d7
NazmulIslamTuhin <nazmulislam640050@gmail.com>
2023-05-13 09:55:14 +0600

    text-have-new-problem

--- lib/main.dart
+++ lib/main.dart
@@ -952,7 +952,7 @@ class _MyHomePageState extends State<MyHomePage> {
                           mainAxisAlignment: MainAxisAlignment.center,
                           children: [
                             Text(
-                              "Report an Issue fshev",
+                              "Report an Issue fshe",
                               style: TextStyle(
                                   fontSize: 25,
                                   color: Colors.red,
