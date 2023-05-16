aa4a30de468bad0a8e40aa75bc6ef7559d299675
NazmulIslamTuhin <nazmulislam640050@gmail.com>
2023-05-17 04:59:57 +0600

    side-gap-problem-solving

--- lib/main.dart
+++ lib/main.dart
@@ -452,7 +452,7 @@ class _MyHomePageState extends State<MyHomePage> {
                     children: [
                       Container(
                         height: 150,
-                        width: 360,
+                        width: 350,
                         decoration: BoxDecoration(
                             image: DecorationImage(
                                 image: AssetImage(
