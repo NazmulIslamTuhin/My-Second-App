ec1c678b5cf0d11b4071dfc194ee9e4cffff9277
NazmulIslamTuhin <nazmulislam640050@gmail.com>
2023-05-29 08:12:21 +0600

    -done

--- lib/main.dart
+++ lib/main.dart
@@ -82,7 +82,7 @@ class _MyHomePageState extends State<MyHomePage> {
                     mainAxisAlignment: MainAxisAlignment.center,
                     children:  [
                       Text(
-                        "Rajshahi",
+                        "Rajshahi ",
                         style: TextStyle(
                             color: Colors.white70,
                             fontSize: 40,
