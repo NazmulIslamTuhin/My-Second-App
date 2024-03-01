dc81db77b3717651c3469e9ec244214fc3890ee3
NazmulIslamTuhin <nazmulislam640050@gmail.com>
2024-03-01 22:51:58 +0600

    secondApp-name-done

--- lib/main.dart
+++ lib/main.dart
@@ -25,7 +25,7 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Weather'),
+      home: const MyHomePage(title: 'Flutter'),
     );
   }
 }
