0d92f6944ffe5f17067324a300a1af1412dd50cc
NazmulIslamTuhin <nazmulislam640050@gmail.com>
2024-10-23 21:54:16 +0600

    carousel-project-start

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 import 'NavBar.dart';
 
@@ -88,6 +89,25 @@ class _MyHomePageState extends State<MyHomePage> {
                     height: 15),
 
                   //Top container start
+
+                  CarouselSlider(
+                    options: CarouselOptions(height: 100.0),
+                    items: [1,2,3,4,5].map((i) {
+                      return Builder(
+                        builder: (BuildContext context) {
+                          return Container(
+                              width: MediaQuery.of(context).size.width,
+                              margin: EdgeInsets.symmetric(horizontal: 5.0),
+                              decoration: BoxDecoration(
+                                  color: Colors.amber
+                              ),
+                              child: Text('text $i', style: TextStyle(fontSize: 16.0),)
+                          );
+                        },
+                      );
+                    }).toList(),
+                  ),
+                  SizedBox(height: 10,),
 
                   Container(
                     decoration: BoxDecoration(
